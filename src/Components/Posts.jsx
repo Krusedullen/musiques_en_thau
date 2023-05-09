fc4a19704b79aeb31390bfdf2965cf9ede95a1bc
Heirin <eirinbjorknes@gmail.com>
2023-05-09 21:56:39 +0200

    move styles to src folder

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "../../public/styles/layout/Posts.css";
+import "../styles/layout/Posts.css";
 
 export default function Posts(props) {
   return (
     <div className="posts">
-      <h2 className="post-header">Posts</h2>
+      <h2 className="post-header"> Posts </h2>{" "}
     </div>
   );
 }
