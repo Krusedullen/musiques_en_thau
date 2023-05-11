01d8d9fa911bdca89b8094f02f52aea1fe748d53
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2023-05-11 22:23:51 +0200

    add utils

--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "../Images/Msign.png";
 import "../styles/layout/MainHeader.scss";
+import {figmaSkisse} from "../Utils";
 
 export default function Header(props) {
   return (
-    <header className="App-header">
+    <header className="Main-header">
       <img className="logo" src={logo} alt="logo" />
+      <nav className="menu">
+        <a href={figmaSkisse}> figma skisse </a>
+      <a href="https://www.bt.no/">bt </a></nav>
     </header>
   );
 }
