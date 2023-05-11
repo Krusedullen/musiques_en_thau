98ce937b7898112ab96317b5f9b9de005c4fe223
Heirin <eirinbjorknes@gmail.com>
2023-05-11 20:20:58 +0200

    add sass variables file

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,7 @@
-declare module '*.mp4' {
-    const src: string;
-    export default src;
-  }
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
