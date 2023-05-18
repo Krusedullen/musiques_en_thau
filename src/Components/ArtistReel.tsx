4725cdb0f369ad3ddcedce332946a07a2986cdca
eirin-bjorknes <eirin.bjorknes@schibsted.com>
2023-05-18 15:32:29 +0200

    refactor ArtistReel

--- src/Components/ArtistReel.tsx
+++ src/Components/ArtistReel.tsx
@@ -1,26 +1,21 @@
 import React from "react";
+import ArtistCard from "./ArtistCard";
+import { artists } from "../Utils/artists";
 
 export default function ArtistReel(): JSX.Element {
+  const artistCards = Object.values(artists).map(({navn, bio})=> {
+    return (
+      <ArtistCard name={navn} bio={bio} />
+    );
+  });
+
   return (
     <div className="artist-Reel">
       <h1 className="reel-header">Meet The Artists</h1>
       <div className="artists">
-        <div className="artist-card">
-          Dette kortet skal vi gjøre om til en egen komponent og hente inn
-          artistene på samme måte som med posts.
-        </div>
-        <div className="artist-card">
-          Dette kortet skal vi gjøre om til en egen komponent og hente inn
-          artistene på samme måte som med posts.
-        </div>
-        <div className="artist-card">
-          Dette kortet skal vi gjøre om til en egen komponent og hente inn
-          artistene på samme måte som med posts.
-        </div>
-        <div className="artist-card">
-          Dette kortet skal vi gjøre om til en egen komponent og hente inn
-          artistene på samme måte som med posts.
-        </div>
+        <>
+        {artistCards}
+        </>
       </div>
     </div>
   );
