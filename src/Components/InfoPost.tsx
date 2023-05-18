cdc6492552fc2ff2b1607b87caa106833d6f56e5
Therese Skaar Bjorknes <thereseskaar@hotmail.com>
2023-05-18 14:45:53 +0200

    make infopost Component

---
+++ src/Components/InfoPost.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type infoPostProps = {
+    title: string;
+    description: string;
+
+}
+
+const InfoPost: React.FC<infoPostProps> = ({title, description}) =>
+{
+  return (
+    <div className="info-post">
+      <h1 className="info-post-header">{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default InfoPost;
---
+++ src/Utils/entries.ts
@@ -0,0 +1,14 @@
+export const entries = {
+    "entry": {
+        "key": "entry0",
+        "title": "sometitle",
+        "description" : "content",
+    },
+
+    "entry1": {
+        "key": "entry1",
+        "title": "sometitle",
+        "description" : "content",
+    }
+
+}
