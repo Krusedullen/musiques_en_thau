149158af4bce4b28e9acdf961effb7ab8d0b7b89
Therese Skaar Bjorknes <thereseskaar@hotmail.com>
2023-05-18 15:28:38 +0200

    make posts iterate entries

--- src/Components/Posts.tsx
+++ src/Components/Posts.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {entries} from "../Utils/entries.json"
+import {entries} from "../Utils/entries"
+import InfoPost from "./InfoPost";
 
 export default function Posts() {
   return (
     <div className="posts">
-      
-    </div>
-  );
-}
+      { Object.values(entries).map(({title, description}) => {
+        return (<InfoPost title={title} description={description}/>)
+        })
+      } </div>
+    )};
