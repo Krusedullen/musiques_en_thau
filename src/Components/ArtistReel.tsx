64a3c82b303a12848a498092b00d8e9883736d5f
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-03 23:51:18 +0200

    added google map location

--- src/Components/ArtistReel.tsx
+++ src/Components/ArtistReel.tsx
@@ -23,6 +23,14 @@ export default function ArtistReel(): JSX.Element {
       <div className="artist-reel__artists">
         <>{artistCards}</>
       </div>
+
+      <div className="side-bar-map">
+        <iframe
+          title="Marseillan Map"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1450.5510027831413!2d3.530230088037457!3d43.35397685563521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sno!4v1685827295465!5m2!1sen!2sno"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   );
 }
