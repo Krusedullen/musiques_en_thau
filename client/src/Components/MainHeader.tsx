7785f55fe8f94ed87167a8c3d95b8df6519dda79
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-14 14:39:27 +0200

    add translate link

--- client/src/Components/MainHeader.tsx
+++ client/src/Components/MainHeader.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import logo from "../Images/Msign.png";
-import { musiquesFacebook } from "../Utils/Utils";
+import { musiquesFacebook, translateFrench } from "../Utils/Utils";
 
 export default function Header() {
   return (
     <header className="main-header">
       <img className="logo" src={logo} alt="logo" />
       <nav className="menu">
+        <a href={translateFrench}> French/ le français </a>
         <a href={musiquesFacebook}> Facebook </a>
-        
       </nav>
     </header>
   );
--- client/src/Utils/Utils.ts
+++ client/src/Utils/Utils.ts
@@ -1,5 +1,9 @@
-
 // Test Links
-export const figmaSkisse = "https://www.figma.com/file/GWSCCoPncT0HAB1rPylL0v/CMS-Musiques-en-Thau?type=design&node-id=0-1&t=OmjNLWcy1F19TzZj-0";
+export const figmaSkisse =
+  "https://www.figma.com/file/GWSCCoPncT0HAB1rPylL0v/CMS-Musiques-en-Thau?type=design&node-id=0-1&t=OmjNLWcy1F19TzZj-0";
 
-export const musiquesFacebook = "https://www.facebook.com/groups/3297190587162445"
+export const musiquesFacebook =
+  "https://www.facebook.com/groups/3297190587162445";
+
+export const translateFrench =
+  "https://musiquesenthau-com.translate.goog/?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en-US&_x_tr_pto=wapp";
