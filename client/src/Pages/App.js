7aeb3a0712c47d56b5c3a41847d1627a0c198ae3
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-15 00:46:13 +0200

    cache clear code in app.js

--- client/src/Pages/App.js
+++ client/src/Pages/App.js
@@ -4,6 +4,17 @@ import Posts from "../Components/Posts";
 import ArtistReel from "./../Components/ArtistReel";
 
 function App() {
+  const clearCacheData = () => {
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name);
+      });
+    });
+    console.log("Complete Cache Cleared");
+  };
+
+  clearCacheData();
+  
   return (
     <div className="App">
       <Header />
