7c71ccac5b33f75d3b0c9df29ea4bbda485b9676
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-18 02:46:53 +0200

    cache-control header options

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 5000;
 
+var options = {
+  //etag: false,
+  maxAge: 300,
+};
+
 const app = express();
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")), options);
+
+//http-header with cache control:  res.set("Cache-Control", "no-store") eller res.append()
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("/", function (req, res) {
   //response
-  res.cache
-    .clear()
-    .sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
