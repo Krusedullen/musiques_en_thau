aa6e4656d3b3723e3210bac61a4a3a35b1a31a3e
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-18 03:22:07 +0200

    uppdate middleware

--- server/index.js
+++ server/index.js
@@ -2,29 +2,35 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 5000;
 
+/*
 var options = {
-  //etag: false,
+  etag: false,
   maxAge: 600,
 };
+*/
 
 const app = express();
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../client/build")), options);
+
+//cache control to disable cache.
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
+/*
+Priority serve any static files. This has its own cache system. may enable when site becomes more dynamic.
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+*/
 
 //http-header with cache control:  res.set("Cache-Control", "no-store") eller res.append()
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("/", function (req, res) {
   //response
+  res.set("Cache-Control", "no-store");
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
-
-
 
 /*
 const clearCacheData = () => {
