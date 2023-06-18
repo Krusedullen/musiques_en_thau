1cfee4388b9bd55f0688345f34394625fdb16442
Therese S. Bjørknes <thereseskaar@hotmail.com>
2023-06-18 17:27:16 +0200

    still figuring out cache headers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const nocache = require('nocache');
 const port = process.env.PORT || 5000;
 
 /*
@@ -11,22 +12,15 @@ var options = {
 
 const app = express();
 
-/*
-//cache control to disable cache.
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store");
-  next();
-});
-*/
-
-//Priority serve any static files. This has its own cache system. may enable when site becomes more dynamic.
+app.use(nocache());
+app.set('etag', false);
+//Priority serve any static files. This has its own cache system.
 app.use(
   express.static(path.resolve(__dirname, "../client/build"), {
     maxage: "5000", // uses milliseconds per docs
   })
 );
 
-//http-header with cache control:  res.set("Cache-Control", "no-store") eller res.append()
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("/", function (req, res) {
@@ -36,3 +30,14 @@ app.get("/", function (req, res) {
 
 //listener on port
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+//http-header with cache control:  res.set("Cache-Control", "no-store") eller res.append()
+/*
+//cache control to disable cache.
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+*/
