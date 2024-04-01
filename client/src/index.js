3e00dcf591415e284624a223738649139a081fd8
Therese S. Bjørknes <thereseskaar@hotmail.com>
2024-04-01 16:24:43 +0200

    Add new path for North to South Festival

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./Pages/App";
 import ErrorPage from "./Components/ErrorPage";
 import MainPage from "./Components/MainPage";
+import NorthSouth from './Components/NorthSouth';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <MainPage />,
+      },
+      {
+        path: "northtosouth",
+        element: <NorthSouth />,
       },
     ],
   },
